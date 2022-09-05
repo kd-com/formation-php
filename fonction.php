0f4c7eb43aae7bfbddfa3ed36cbd7d8e300a3817
kd-com <contact@kd-com.fr>
2022-09-05 17:04:11 +0200

    first commit

---
+++ fonction.php
@@ -0,0 +1,25 @@
+<?php
+// variable + fonction
+    $produit_find = "Xiaomi";
+    $pos = strpos($produit1[0], strtoupper($produit_find));
+    echo $pos;
+
+    if( strtoupper($produit1[0]) == $pos) {
+        echo "C'est la marque Xiaomi !";
+    } else {
+        echo "c'est une autre marque !";
+    }
+
+    echo "<br>";
+    
+    
+    // affichage age
+    echo"<br>";
+    $age=30;
+    if($age > 1) {
+        echo "majeur !";
+    } else {
+        echo "mineur !";
+    }
+    ?>
+    <br>
