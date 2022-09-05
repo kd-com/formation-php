6e3923a7e49c4a73b87409569df35dbbc6755da3
kd-com <contact@kd-com.fr>
2022-09-05 17:20:28 +0200

    first commit

--- function.php
+++ function.php
@@ -109,10 +109,4 @@
         echo $value. " / ";
     }
 
-    // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1+2+3+4+5
-    // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1x2x3x4x5
-    // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le calcul et le resultat de 1+2+3+4+5
-    // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le calcul et le resultat de 1x2x3x4x5
-
-
      ?>
