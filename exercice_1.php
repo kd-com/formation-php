1919b92731ced5144e4314db6a7a4d04338d41d1
kd-com <contact@kd-com.fr>
2022-09-05 17:48:20 +0200

    suppresion de commentaire

--- exercice_1.php
+++ exercice_1.php
@@ -1,5 +1,5 @@
 <?php
-// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1+2+3+4+5 - test commit 3
+// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1+2+3+4+5
 
     // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1x2x3x4x5
 
