f7df45cefeb2a2a8d2e2ce4d524410de22e1c4a1
kd-com <contact@kd-com.fr>
2022-09-05 17:44:16 +0200

    first commit

--- exercice_1.php
+++ exercice_1.php
@@ -1,6 +1,10 @@
 <?php
-// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1+2+3+4+5
+// créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1+2+3+4+5 - test commit
+
     // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le resultat de 1x2x3x4x5
+
     // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le calcul et le resultat de 1+2+3+4+5
+
     // créer une fonction qui prend en parametre un nombre et qui retourne ex: 5 retourne le calcul et le resultat de 1x2x3x4x5
+
     ?>
